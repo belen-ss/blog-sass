9b92f32240f3900e4bcfde49240fb5cbfc942ef5
Astrid Belén <asosa078@accitesz.com>
2021-06-11 13:04:58 -0500

    responsive fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Ecommerce Lelenz",
   },
-  plugins: [
-    "gatsby-plugin-sass",
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto`,
-            variants: [`400`, `700`]
-          },
-          {
-            family: `Roboto`,
-            subsets: [`latin`]
-          },
-        ],
-      },
-    }  
-  ],
-
+  plugins: ["gatsby-plugin-sass"],
 };
